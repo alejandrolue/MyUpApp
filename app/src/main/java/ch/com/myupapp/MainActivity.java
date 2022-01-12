a49fb0eda67fd73ea917e7d73f7766c2f428b58f
alejandro lüthi <alejandro.luthi@gmail.com>
2022-01-12 14:57:28 +0100

    Added calls for changes in the search bar

--- app/src/main/java/ch/com/myupapp/MainActivity.java
+++ app/src/main/java/ch/com/myupapp/MainActivity.java
@@ -6,12 +6,20 @@ import androidx.appcompat.widget.SearchView;
 import android.app.SearchManager;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int TRIGGER_AUTO_COMPLETE = 100;
+    private static final long AUTO_COMPLETE_DELAY = 300;
+
     private SuggestService suggest;
     private boolean isSelectedFromList;
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.options_menu, menu);
+        inflater.inflate(R.menu.search_menu, menu);
 
         // Associate searchable configuration with the SearchView
         SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
@@ -38,10 +46,60 @@ public class MainActivity extends AppCompatActivity {
         mSearchAutoComplete.setDropDownHeight(1300);
         mSearchAutoComplete.setOnItemClickListener((parent, view, position, id) -> {
 
-//            String query = (String)parent.getItemAtPosition(position);
 
             mSearchAutoComplete.setText(suggest.getObject(position));
             isSelectedFromList = true;
         });
-}
+        mSearchAutoComplete.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int
+                    count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before,
+                                      int count) {
+                isSelectedFromList = false;
+                handler.removeMessages(TRIGGER_AUTO_COMPLETE);
+                handler.sendEmptyMessageDelayed(TRIGGER_AUTO_COMPLETE,
+                        AUTO_COMPLETE_DELAY);
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                if (!isSelectedFromList){
+
+                    return false;
+                }
+                String[] input = query.split("-");
+                redirectToDetails(input[0]);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                isSelectedFromList = false;
+                return false;
+            }
+        });
+
+        handler = new Handler(msg -> {
+            if (msg.what == TRIGGER_AUTO_COMPLETE) {
+
+                String input = mSearchAutoComplete.getText().toString();
+
+                if (!TextUtils.isEmpty(mSearchAutoComplete.getText()) && input.length() >= 3) {
+
+                    sendApiCall(input);
+                }
+            }
+            return false;
+        });
+
+        return true;
+    }
 }
