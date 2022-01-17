d8a2c996ef060c634465748922fa19fb8a1ebb50
alejandro lüthi <alejandro.luthi@gmail.com>
2022-01-17 16:13:29 +0100

    Request changes and made tried to make notifications work. List still not showing added stocks. And Notification not working properly in testing. Needs some fixes

--- app/src/main/java/ch/com/myupapp/MainActivity.java
+++ app/src/main/java/ch/com/myupapp/MainActivity.java
@@ -2,36 +2,34 @@ package ch.com.myupapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.SearchManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
 import android.os.IBinder;
 import android.preference.PreferenceManager;
-import android.text.Editable;
-import android.text.TextUtils;
-import android.text.TextWatcher;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.android.volley.Response;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
@@ -40,7 +38,11 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<Stock> stocks = new ArrayList<>();
     RecyclerView recyclerView;
     boolean mBound = false;
+    Button button;
+    Button notifyBtn;
+    StockViewAdapter stockViewAdapter;
 
+    @SuppressLint("ObsoleteSdkInt")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,23 +50,55 @@ public class MainActivity extends AppCompatActivity {
 
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
         tickers = preferences.getStringSet("tickers", new HashSet<>());
-        Set<String> copyTickers = new HashSet<>(tickers);
+        Set<String> newTickers = new HashSet<>(tickers);
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             String ticker = extras.getString("ticker");
             boolean delete = extras.getBoolean("delete");
             if(!delete){
-                copyTickers.add(ticker);
-                preferences.edit().putStringSet("tickers", copyTickers).apply();
+                newTickers.add(ticker);
+                preferences.edit().putStringSet("tickers", newTickers).apply();
                 tickers.add(ticker);
             } else {
-                copyTickers.remove(ticker);
-                preferences.edit().putStringSet("tickers", copyTickers).apply();
+                newTickers.remove(ticker);
+                preferences.edit().putStringSet("tickers", newTickers).apply();
                 tickers.remove(ticker);
             }
         }
+
+
+        //Notification Funktioniert nicht ganz
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("Up", "Up", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+
+        notifyBtn = findViewById(R.id.test_notification);
+        notifyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Notification.Builder builder = new Notification.Builder(MainActivity.this,"Stock Update");
+                builder.setContentTitle("UP");
+                builder.setContentText("Test Text");
+                builder.setSmallIcon(R.mipmap.ic_launcher);
+                builder.setAutoCancel(true);
+
+                NotificationManagerCompat managerCompat = NotificationManagerCompat.from(MainActivity.this);
+                managerCompat.notify(1, builder.build());
+            }
+        });
+
+
+
+        button = findViewById(R.id.add_stock);
+        button.setOnClickListener(e -> {
+            Intent intent = new Intent(this, AddStock.class);
+            startActivity(intent);
+        });
     }
+
     private void addStockView(String ticker) {
         if (mBound) {
             getData(ticker);
@@ -74,7 +108,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void getData(String ticker) {
-        String url = "https://api.tiingo.com/tiingo/daily/" + ticker + "/prices?startDate=2021-11-20&endDate=2021-11-27&columns=date,close";
+        String url = "https://api.tiingo.com/tiingo/daily/" + ticker + "/prices?startDate=2022-01-01&endDate=2022-01-16&columns=date,close";
         mService.doRequest(url, json -> {
             try {
                 Stock stock = new Stock();
@@ -82,9 +116,12 @@ public class MainActivity extends AppCompatActivity {
                 stock.setJsonChartData(json);
                 stocks.add(stock);
 
+                stockViewAdapter = new StockViewAdapter(stocks);
+
                 recyclerView = findViewById(R.id.recyclerView);
                 recyclerView.setLayoutManager(new LinearLayoutManager(this));
                 recyclerView.setItemAnimator(new DefaultItemAnimator());
+                recyclerView.setAdapter(stockViewAdapter);
             } catch (Exception e) {
                 e.printStackTrace();
             }
